3571d8c1bded5da8669c3f8749fba3ca8dbcd610
danx0r <dbm@dbm-VPCF116FX.(none)>
2013-02-16 16:00:19 -0800

    gitup

--- checkArdour.py
+++ checkArdour.py
@@ -5,7 +5,7 @@ check this directory's ardour and .wav timestamps for up-to-dateness
 """
 
 import os, sys, time
-MAXT = 3600*2
+MAXT = 3600*5
 
 path = os.path.abspath(".")
 segname = os.path.basename(path)
@@ -20,7 +20,23 @@ tr = os.path.getmtime("SEG.ardour".replace("SEG", segname))
 ##print ta,tb,tc,t_,tr
 
 mx = max(t_, ta, tb, tc, tr) - min(t_, ta, tb, tc, tr)
-if mx < MAXT:
-    print "looks up to date; max time:", mx / 3600., "hours"
+chk = True
+if t_ < ta:
+    print "_Session older than a_Session",
+    chk = False
+if t_ < tb:
+    print "_Session older than b_Session",
+    chk = False
+if t_ < tc:
+    print "_Session older than c_Session",
+    chk = False
+if t_ < tr:
+    print "_Session older than ardour",
+    chk = False
+
+if mx > MAXT:
+    print "suspicious:", mx / 3600., "hours",
+if chk == True:
+    print "looks up to date!"
 else:
-    print "needs RECOMPILE! max time:", mx / 3600., "hours"
+    print "needs RECOMPILE!"
